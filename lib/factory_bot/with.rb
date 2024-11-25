82c83b162ba9fb686f650c403db450066dbf4a7f
yubrot <yubrot@gmail.com>
2024-11-25 22:55:15 +0900

    bundle gem factory_bot-with

---
+++ lib/factory_bot/with.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "with/version"
+
+module FactoryBot
+  module With
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/factory_bot/with_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe FactoryBot::With do
+  it "has a version number" do
+    expect(FactoryBot::With::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
